478b6ff5105643d779608146f874811d9421b25d
pntt <pntt220200@gmail.com>
2021-01-10 02:31:24 +0700

    Add Load more

--- HubbleSpace_Final/Controllers/HomeController.cs
+++ HubbleSpace_Final/Controllers/HomeController.cs
@@ -71,48 +71,86 @@ namespace HubbleSpace_Final.Controllers
         {
             return View();
         }
-        public async Task<IActionResult> Categories(string Object="", string Name="")
+        
+        public async Task<IActionResult> Categories(int CountForTake = 1, string Object="", string Name="")
         {
+            int take = 5;
+            int toltal_take = (int)Math.Ceiling((double)_context.Color_Product
+                                                        .Include(p => p.product)
+                                                        .Include(p => p.product.category)
+                                                        .Where(p => p.product.category.Object.Contains(Object))
+                                                        .Where(p => p.product.category.Category_Name.Contains(Name))
+                                                        .Count() / take);
+
+            var Color_Products = _context.Color_Product.Include(p => p.product)
+                                                       .Include(p => p.product.category)
+                                                       .Where(p => p.product.category.Object.Contains(Object))
+                                                       .Where(p => p.product.category.Category_Name.Contains(Name))
+                                                       .OrderBy(p => p.Date)
+                                                       .Take(CountForTake * take)
+                                                       .ToListAsync();
+            ViewData["toltal_take"] = toltal_take;
+            ViewData["CountForTake"] = CountForTake + 1;
+            ViewData["products_taked"] = take * CountForTake;
             ViewData["Object"] = Object;
             ViewData["Name"] = Name;
-            return View(await _context.Color_Product.Include(p => p.product)
-                                                    .Include(p => p.product.category)
-                                                    .Where(p => p.product.category.Object.Contains(Object))
-                                                    .Where(p => p.product.category.Category_Name.Contains(Name))
-                                                    .ToListAsync());
+            return View(await Color_Products);
         }
 
         public IActionResult Search()
         {
-            var search = Request.Form["Search"];
+            var search = Request.Form["Search"].ToString().ToLower();
             ViewData["Object"] = "Search";
             ViewData["Name"] = "";
             if (search == "")
             {
                 return View("Categories", _context.Color_Product.Include(p => p.product));
             }
-            return View("Categories", _context.Color_Product.Where(m => m.product.Product_Name.Contains(search)).Include(p => p.product));
+            return View("Categories", _context.Color_Product.Where(m => m.product.Product_Name.ToLower().Contains(search)).Include(p => p.product));
         }
 
         [HttpPost]
         public IActionResult Filter()
         {
-            Console.WriteLine(Request.Form["brand"]);
             if (Request.Form["brand"] == "Adidas")
             {
+                ViewData["Object"] = Request.Form["brand"];
                 return View("Categories", _context.Color_Product.Include(p => p.product).Where(p => p.product.Brand.Brand_Name == "Adidas"));
             }
             if (Request.Form["brand"] == "Nike")
             {
+                ViewData["Object"] = Request.Form["brand"];
                 return View("Categories", _context.Color_Product.Include(p => p.product).Where(p => p.product.Brand.Brand_Name == "Nike"));
             }
             if (Request.Form["brand"] == "Reebok")
             {
+                ViewData["Object"] = Request.Form["brand"];
                 return View("Categories", _context.Color_Product.Include(p => p.product).Where(p => p.product.Brand.Brand_Name == "Reebok"));
             }
             if (Request.Form["brand"] == "Puma")
             {
+                ViewData["Object"] = Request.Form["brand"];
                 return View("Categories", _context.Color_Product.Include(p => p.product).Where(p => p.product.Brand.Brand_Name == "Puma"));
+            }
+            if (Request.Form["price"] == "0")
+            {
+                ViewData["Object"] = Request.Form["price"];
+                return View("Categories", _context.Color_Product.Include(p => p.product).Where(p => p.product.Price_Sale < 1000000));
+            }
+            if (Request.Form["price"] == "1000000")
+            {
+                ViewData["Object"] = Request.Form["price"];
+                return View("Categories", _context.Color_Product.Include(p => p.product).Where(p => p.product.Price_Sale >= 1000000 && p.product.Price_Sale < 3000000));
+            }
+            if (Request.Form["price"] == "3000000")
+            {
+                ViewData["Object"] = Request.Form["price"];
+                return View("Categories", _context.Color_Product.Include(p => p.product).Where(p => p.product.Price_Sale >= 3000000 && p.product.Price_Sale < 5000000));
+            }
+            if (Request.Form["price"] == "5000000")
+            {
+                ViewData["Object"] = Request.Form["price"];
+                return View("Categories", _context.Color_Product.Include(p => p.product).Where(p => p.product.Price_Sale >= 5000000));
             }
             return View("Categories", _context.Color_Product.Include(p => p.product));
         }
@@ -121,6 +159,7 @@ namespace HubbleSpace_Final.Controllers
         {
             return View(await _context.Img_Product.Include(p => p.color_Product.product)
                                                   .Include(p => p.color_Product.product.category)
+                                                  .Include(p => p.color_Product.product.Brand)
                                                   .Where(p => p.ID_Color_Product == id)
                                                   .ToListAsync());
         }
@@ -132,12 +171,13 @@ namespace HubbleSpace_Final.Controllers
         {
             return PartialView(await _context.Color_Product.Where(p => p.ID_Product == id).ToListAsync());
         }
-        public async Task<IActionResult> GetRecommendProducts(string name="", string _object ="")
+        public async Task<IActionResult> GetRecommendProducts(string Object="", string Name ="", string Brand_Name = "")
         {
             return PartialView(await _context.Color_Product.Include(p => p.product)
                                                     .Include(p => p.product.category)
-                                                    .Where(p => p.product.category.Object.Contains(_object))
-                                                    .Where(p => p.product.category.Category_Name.Contains(name))
+                                                    .Where(p => p.product.category.Object.Contains(Object))
+                                                    .Where(p => p.product.category.Category_Name.Contains(Name))
+                                                    .Where(p => p.product.Brand.Brand_Name.Contains(Brand_Name))
                                                     .ToListAsync());
         }
 
